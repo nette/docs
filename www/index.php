b4f5b088cbfaf0a8e9012933dc31a961e14ede5c
Filip Procházka <filip@prochazka.su>
2013-08-17 14:46:45 +0200

    Preview: configurable refresh rate

--- www/index.php
+++ www/index.php
@@ -8,7 +8,7 @@ require __DIR__ . '/../src/Convertor.php';
 
 if (empty($_GET['file'])) {
 	$dir = htmlspecialchars(realpath(__DIR__ . '/..'));
-	$url = htmlspecialchars('http://' . $_SERVER['SERVER_NAME'] . $_SERVER['SCRIPT_NAME'] . '?lang=cs&file=writing');
+	$url = htmlspecialchars('http://' . $_SERVER['SERVER_NAME'] . $_SERVER['SCRIPT_NAME'] . '?lang=cs&file=writing&refresh=10');
 
 	echo <<<HTML
 <meta charset="utf-8">
@@ -24,13 +24,14 @@ HTML;
 $file = $_GET['file'] . '.texy';
 $book = 'doc-2.0';
 $lang = !empty($_GET['lang']) ? $_GET['lang'] : 'cs';
+$autorefresh = !empty($_GET['refresh']) ? json_encode($_GET['refresh']) : '0';
 
 $header = <<<HTML
 <!doctype html>
 <head>
   <meta charset="utf-8">
   <link rel="stylesheet" href="http://files.nette.org/css/combined.css">
-  <script> setTimeout(function () { window.location.reload(); }, 5000) </script>
+  <script> if ($autorefresh) { setTimeout(function () { window.location.reload(); }, parseInt($autorefresh) * 1000); } </script>
 </head>
 <body><div id=page><div id=main class=sidebar><div id=content>
 HTML;

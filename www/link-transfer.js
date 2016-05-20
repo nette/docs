d05196e5298e00b7b11cd85efac397010372dee3
Michal Kalita <kalita.michal@gmail.com>
2016-05-20 11:27:48 +0200

    Used tabulators in file link-transfer.js

--- www/link-transfer.js
+++ www/link-transfer.js
@@ -1,20 +1,20 @@
 // all links to documentation will be linked to local server
 function linkTransfer(e) {
-    var e = window.e || e;
+	var e = window.e || e;
 
-    if (e.target.tagName !== 'A')
-        return;
+	if (e.target.tagName !== 'A')
+		return;
 
-    var re = /^\/([\w]+)\/([\w\.\/\-]+)([#\w\-]*)$/;
-    var m;
-    if ((m = re.exec(e.target.getAttribute('href'))) !== null) {
-        e.preventDefault();
-        window.open(window.location.protocol + "//" + window.location.host + window.location.pathname
-            + "?lang=" + m[1] + "&file=" + m[2] + m[3], "_self");
-    }
+	var re = /^\/([\w]+)\/([\w\.\/\-]+)([#\w\-]*)$/;
+	var m;
+	if ((m = re.exec(e.target.getAttribute('href'))) !== null) {
+		e.preventDefault();
+		window.open(window.location.protocol + "//" + window.location.host + window.location.pathname
+			+ "?lang=" + m[1] + "&file=" + m[2] + m[3], "_self");
+	}
 }
 
 if (document.addEventListener)
-    document.addEventListener('click', linkTransfer, false);
+	document.addEventListener('click', linkTransfer, false);
 else
-    document.attachEvent('onclick', linkTransfer);
+	document.attachEvent('onclick', linkTransfer);
